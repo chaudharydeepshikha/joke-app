5a395da06a26907707ff038a00c320d2e2ebeb2e
Deepshikha <Deepshikha.Chaudhary@tatacommunications.com>
2017-09-23 15:31:00 +0530

    Java configuration (056)

--- src/main/java/deepshikha/spring/jokesapp/services/JokeServiceImpl.java
+++ src/main/java/deepshikha/spring/jokesapp/services/JokeServiceImpl.java
@@ -1,14 +1,17 @@
 package deepshikha.spring.jokesapp.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JokeServiceImpl implements JokeService {
 
+    @Autowired
+    private ChuckNorrisQuotes chuckNorrisQuotes;
+
     @Override
     public String getRandomJoke() {
-        ChuckNorrisQuotes chuckNorrisQuotes = new ChuckNorrisQuotes();
         return chuckNorrisQuotes.getRandomQuote();
     }
 }
